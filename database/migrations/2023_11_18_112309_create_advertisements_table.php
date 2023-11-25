ef511ac492c3d7d19394d2747d359e19c02ab79d
Amstrong1 <lewisamstrongh@gmail.com>
2023-11-25 12:29:27 +0100

    advertisement migration updated

--- database/migrations/2023_11_18_112309_create_advertisements_table.php
+++ database/migrations/2023_11_18_112309_create_advertisements_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('advertisements', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->string('image');
             $table->string('link')->nullable();
             $table->boolean('show');
